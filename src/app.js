9b65994e0934e0c00b7a8a2db384433d8be25ace
Zasqia Oktavina <zasqiavinaa@gmail.com>
2025-12-03 17:53:38 +0700

    Tambah src, tests, dan konfigurasi

---
+++ src/app.js
@@ -0,0 +1,20 @@
+function tambah(a, b) {
+  return a + b;
+}
+
+function kurang(a, b) {
+  return a - b;
+}
+
+function kali(a, b) {
+  return a * b;
+}
+
+function bagi(a, b) {
+  if (b === 0) {
+    throw new Error('Tidak bisa dibagi nol!');
+  }
+  return a / b;
+}
+
+module.exports = { tambah, kurang, kali, bagi };
---
+++ tests/app.test.js
@@ -0,0 +1,31 @@
+const { tambah, kurang, kali, bagi } = require('../src/app');
+
+// Test untuk fungsi tambah
+describe('Test Fungsi Kalkulator', () => {
+  
+  test('Fungsi tambah harus mengembalikan hasil penjumlahan', () => {
+    expect(tambah(2, 3)).toBe(5);
+    expect(tambah(10, 5)).toBe(15);
+    expect(tambah(-1, 1)).toBe(0);
+  });
+
+  test('Fungsi kurang harus mengembalikan hasil pengurangan', () => {
+    expect(kurang(10, 3)).toBe(7);
+    expect(kurang(5, 10)).toBe(-5);
+  });
+
+  test('Fungsi kali harus mengembalikan hasil perkalian', () => {
+    expect(kali(3, 4)).toBe(12);
+    expect(kali(-2, 5)).toBe(-10);
+  });
+
+  test('Fungsi bagi harus mengembalikan hasil pembagian', () => {
+    expect(bagi(10, 2)).toBe(5);
+    expect(bagi(9, 3)).toBe(3);
+  });
+
+  test('Fungsi bagi harus error jika dibagi nol', () => {
+    expect(() => bagi(10, 0)).toThrow('Tidak bisa dibagi nol!');
+  });
+
+});
